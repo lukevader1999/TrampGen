bf03d5d5ece6b2fe0f0ac196bb86a48a9213c7f2
Erik <ewalter@students.uni-mainz.de>
2025-02-22 13:31:28 +0100

    Initial commit

---
+++ generateJump.js
@@ -0,0 +1,7 @@
+const Tuchpositionen = ["Stand", "Sitz", "Rücken", "Bauch"];
+const Positionen = ["a", "b", "c", "Grätsche"];
+const Saltorichtung = ["rückwärts", "vorwärts"]
+
+function generateJump() {
+    
+}
