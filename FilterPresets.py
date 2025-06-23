ec3e715e40442c7b4f2a262987afc00ec1471274
Erik <ewalter@students.uni-mainz.de>
2025-06-23 21:04:41 +0200

    Wichtigen Sprung beim Standardfilter ausgeschlossen

--- FilterPresets.py
+++ FilterPresets.py
@@ -2,11 +2,17 @@ from SprungFilter import SprungFilter
 
 exclude_codes = {
     "R R - 0 0 a": "Streckung z. Rücken",
-    "S S - 0 0 a": "Streckung z. Sitz"
+    "S S - 0 0 a": "Streckung z. Sitz",
+    "R B r 2 0 a": "Muffel z. Bauch",
 }
 exclude_codes = exclude_codes.keys()
 defaultPresetFilter = SprungFilter(max_rotationen=2, max_schrauben=1, filter_json_path="filter.json", exclude_codes=exclude_codes)
 
 normaleSaltosPresetFilter = SprungFilter(max_rotationen=4, max_schrauben=1, exclude_codes=exclude_codes)
 
-keinFilterPreset = SprungFilter()
+exclude_codes = {
+    "R R - 0 0 a": "Streckung z. Rücken",
+    "S S - 0 0 a": "Streckung z. Sitz"
+}
+
+keinFilterPreset = SprungFilter(exclude_codes=exclude_codes)
